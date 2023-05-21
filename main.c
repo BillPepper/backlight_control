f0c12ea7bfbf984aad806f593bfcafe872abe25d
Mattes Wenzel <mattes@flawlesslogic.net>
2023-05-22 01:11:57 +0200

    - implement own function to get max brightness - use exit() calls instead of returns - rename some vars

--- main.c
+++ main.c
@@ -17,7 +17,24 @@ void warn_no_root(void) {
 }
 
 void set_backlight(int percent);
-int get_max_backlight(void);
+
+int get_max_brightness(void) {
+  FILE *pFile;
+  int max_brightness;
+
+  pFile = fopen(max_brightness_file, "r");
+
+  if (pFile == 0) {
+    printf("Error, could not open file\n");
+    return 0;
+  }
+
+  fscanf(pFile, "%d", &max_brightness);
+  fclose(pFile);
+
+  return max_brightness;
+};
+
 int file_exists(char *path) {
   int status = access(path, F_OK);
   if (status == 0){
@@ -34,42 +51,35 @@ int main(int argc, char **argv) {
   // 2. Check if arg, brightness in percent
   if (argc != 2){
     printf("Usage: brightness_control <level in percent>\n");
-    return 1;
+    exit(1);
   }
 
   int user_value = atoi(argv[1]);
   if (user_value < 1 || user_value > 100){
     printf("Error, please enter a number between 1 and 100\n");
-    return 2;
+    exit(2);
   }
 
   // x. Check if both files exist
   if (!file_exists(brightness_file)) {
     printf("Error, missing brightness file: %s\n", brightness_file);
-    return 3;
+    exit(3);
   } 
 
   if (!file_exists(max_brightness_file)){
     printf("Error, missing max_brightness file: %s\n", max_brightness_file);
-    return 4;
+    exit(4);
   }
 
   // 3. Load maximum brightness
-  FILE *pFile = fopen(max_brightness_file, "r");
-  if (pFile == 0) {
-    printf("Error, could not open file\n");
-    return 5;
+  int max = get_max_brightness();
+  if (max == 0) {
+    printf("Error, no max brightness found in file\n");
+    exit(5);
   }
 
-  int buff;
-
-  fscanf(pFile, "%d", &buff);
-  fclose(pFile);
-
-
-
   // 4. Calculate new brightness value
-  int new_brightness = buff / 100 * user_value;
+  int new_brightness = max / 100 * user_value;
 
   // 5. Save brightness to file
   FILE *pOut = fopen(brightness_file, "w");
